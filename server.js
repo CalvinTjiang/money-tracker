b96dedf1c28312752247678f3fcdc8d8b6980f3e
Calvin Tjiang <calvin.tjiang@gmail.com>
2019-12-05 16:02:30 +1100

    Add new purchase endpoints: - Update store - Update category  Remove Unnecessary commented code

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect('mongodb://localhost:27017/money-tracker', {useNewUrlParser: tr
 
 });
 
-//Configuring Endpoints
+// Configuring Endpoints
 // User RESTFul endpoionts 
 app.get('/db/users', users.getAll);            // Get all users
 app.get('/db/users/:id', users.getOne);        // Get specific user based on id
@@ -46,25 +46,10 @@ app.delete('/db/stores/:id', stores.deleteOne);// Delete a store
 app.get('/db/categories', categories.getAll);                  // Get all categories
 app.get('/db/categories/super', categories.getWithSuper);      // Get all categories populate with the supercategories
 app.get('/db/categories/:id', categories.getOne);              // Get a specific category based on id
-
 app.put('/db/categories/:id', categories.updateOne);           // Update a category
-
 app.post('/db/categories', categories.createOne);              // Create a new category
-// app.post('/db/categories/super/:id', categories.addSuper);     // Add 1 or more supercategories to a category
-// app.post('/db/categories/sub/:id', categories.addSub);         // Add 1 or more subcategories to a category
-
 app.delete('/db/categories/:id', categories.deleteOne);        // Delete a category
-// app.delete('/db/categories/super/:id', categories.deleteSuper);// Delete 1 or more supercategories from a category
-// app.delete('/db/categories/sub/:id', categories.deleteSub);    // Delete 1 or more subcategories from a category
 
-/*
-app.post('/categories/addSupers/:id', categories.addSupercategories);
-app.post('/categories/addSuper/:id', categories.addSupercategory);
-app.post('/categories/addSubs/:id', categories.addSubcategories);
-app.post('/categories/addSub/:id', categories.addSubcategory);
-app.delete('/categories/deleteSuper/:id/:superId', categories.deleteSupercategory);
-app.delete('/categories/deleteSub/:id/:subId', categories.deleteSubcategory);
-*/
 
 //Note 
 // Add Purchase RESTFul endpoionts with only option which attribute to be populated
@@ -74,3 +59,5 @@ app.get('/db/purchases/:id', purchases.getOne);        // Get a specific purchas
 app.post('/db/purchases', purchases.createOne);        // Create a new purchase
 app.put('/db/purchases/:id', purchases.updateOne);     // Update a purchase
 app.delete('/db/purchases/:id', purchases.deleteOne);  // Delete a purchase
+app.put('/db/purchases/store/:id', purchases.updateStore);
+app.put('/db/purchases/category/:id', purchases.updateCategory);
