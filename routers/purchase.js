e3bae3b48a037436d6a1e68acfa5d97c08892f77
Calvin Tjiang <calvin.tjiang@gmail.com>
2020-01-15 17:04:18 +1100

    add router for each mongoose model
    CRUD:
    - Get All
    - Get One
    - Create One
    - Update One
    - Delete One

---
+++ routers/purchase.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const Purchase = require('../models/purchase');
+
+module.exports = {
+    getAll: function (req, res) {
+        Purchase.find({})
+            .populate('store', { name: 1, icon: 1 })
+            .populate('category', { name: 1, icon: 1 })
+            .sort({date: -1})
+            .exec(function (err, purchases) {
+                if (err) return res.status(400).json(err);
+                if (!purchases) return res.status(404).json();
+                res.json(purchases);
+            });
+    },
+
+    createOne: function (req, res) {
+        let newPurchaseDetails = req.body;
+        newPurchaseDetails._id = new mongoose.Types.ObjectId();
+
+        let purchase = new Purchase(newPurchaseDetails);
+        purchase.save(function (err) {
+            res.json(purchase);
+        });
+    },
+
+    getOne: function (req, res) {
+        Purchase.findOne({ _id: req.params.id })
+            .exec(function (err, purchase) {
+                if (err) return res.status(400).json(err);
+                if (!purchase) return res.status(404).json();
+                res.json(purchase);
+            });
+    },
+
+    updateOne: function (req, res) {
+        Purchase.findOneAndUpdate({ _id: req.params.id }, req.body, function (err, purchase) {
+            if (err) return res.status(400).json(err);
+            if (!purchase) return res.status(404).json();
+            res.json({purchase});
+        });
+    },
+
+    deleteOne: function (req, res) {
+        Purchase.findOneAndRemove({ _id: req.params.id }, function (err, purchase) {
+            if (err) return res.status(400).json(err);
+            res.json(purchase);
+        });
+    },
+};
