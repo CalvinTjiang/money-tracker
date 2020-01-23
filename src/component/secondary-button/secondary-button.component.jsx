bf52eccf135054dbd7f1c24d7bccff65c058b5cf
Calvin Tjiang <calvin.tjiang@gmail.com>
2020-01-23 15:53:30 +1100

    add SecondaryButton component

---
+++ src/component/secondary-button/secondary-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './secondary-button.styles.css';
+
+export const SecondaryButton = (props) =>{
+
+    let className = "btn btn-secondary-color ";
+    // Check for additional className
+    if (props.className !== undefined){
+        className += props.className;
+    }
+
+    // Check for icon properties
+    if (props.icon === undefined) {
+        return (
+            <button className={className} onClick={props.onClick}>
+                <a className="btn-text">{props.children}</a>
+            </button>
+        );
+    }
+
+    return (
+        <button className={className} onClick={props.onClick}>
+            <i className="material-icons icon-secondary-color">{props.icon}</i>
+            <a className="btn-text">{props.children}</a>
+        </button>
+    );
+}
