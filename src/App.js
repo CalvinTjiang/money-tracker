7ab6c0a50aa9d408c04692b4aa2310fad1c460e3
Calvin Tjiang <calvin.tjiang@gmail.com>
2020-01-23 15:54:04 +1100

    add Side Navigation HTML & CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import './index.css';
+
+import { SecondaryButton } from './component/secondary-button/secondary-button.component';
+import { PurchaseContent } from './component/purchase-content/purchase-content.component';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      content: "purchase" 
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <nav id="left-nav">
+          <i id="main-icon" className="material-icons icon-secondary-color">polymer</i>
+
+          <SecondaryButton icon="attach_money">Purchases</SecondaryButton>
+
+          <SecondaryButton icon="people">Users</SecondaryButton>
+
+          <SecondaryButton icon="store">Stores</SecondaryButton>
+
+          <SecondaryButton icon="category">Categories</SecondaryButton>
+          <i className="material-icons icon-secondary-color icon-bottom">settings_applications</i>
+        </nav>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      </div>
+    );
+  }
 }
 
 export default App;
